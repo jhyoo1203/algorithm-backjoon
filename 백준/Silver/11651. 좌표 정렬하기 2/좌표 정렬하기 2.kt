805d4e718ecbfacdfdc09ea6e8039553a3d06a3c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-08-25 12:15:19 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 832 ms, Memory: 78916 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.kt
@@ -0,0 +1,20 @@
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+
+    val points = Array(n) {
+        val st = StringTokenizer(readLine())
+        val x = st.nextToken().toInt()
+        val y = st.nextToken().toInt()
+        x to y
+    }
+
+    points.sortWith(compareBy({ it.second }, { it.first }))
+
+    val sb = StringBuilder()
+    for ((x, y) in points) {
+        sb.append("$x $y\n")
+    }
+    println(sb)
+}
