80d89abf4d12c1ac77607a8789b67caf1762be04
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-09-03 20:09:36 +0900

    [Silver IV] Title: ATM, Time: 100 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.kt
+++ 백준/Silver/11399. ATM/ATM.kt
@@ -3,22 +3,20 @@ import java.util.StringTokenizer
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val st = StringTokenizer(readLine())
-    val people = Array(n) { 0 }
-    var count = 0
+    val people = mutableListOf<Int>()
 
     while (st.hasMoreTokens()) {
-        people[count++] = st.nextToken().toInt()
+        people.add(st.nextToken().toInt())
     }
 
     people.sort()
 
-    val dp = Array(n) { 0 }
-    dp[0] = people.first()
-    var result = people.first()
+    var sum = 0
+    var result = 0
 
-    for (i in 1 until n) {
-        dp[i] = dp[i - 1] + people[i]
-        result += dp[i]
+    people.forEach { person ->
+        sum += person
+        result += sum
     }
 
     print(result)
