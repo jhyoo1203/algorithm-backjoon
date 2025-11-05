4aa7121b86eec911cce4b3ad3c6116203c364233
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-11-05 16:13:03 +0900

    [Silver II] Title: A → B, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int count = 1;
+
+        while (b > a) {
+            if (b % 10 == 1) {
+                b /= 10;
+                count++;
+            } else if (b % 2 == 0) {
+                b /= 2;
+                count++;
+            } else {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        if (b == a) {
+            System.out.println(count);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
